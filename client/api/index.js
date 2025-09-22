ea1d5142939a0ec8b72648f7ddf7dbe109163784
unknown <zafor4558@gmail.com>
2025-09-22 17:42:15 +0600

    Fix Vercel API path and add api directory to client folder

---
+++ client/api/index.js
@@ -0,0 +1,6 @@
+// This file is required for Vercel to properly handle API routes
+// It imports and exports the main server application
+
+const app = require('../../server/index.js');
+
+module.exports = app;
